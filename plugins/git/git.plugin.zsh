b19996090137269e7abb3c5c0637f032add1c066
Ole Christian Rynning <oc@rynning.no>
2010-10-11 09:29:05 +0200

    Removed aliases. I prefer my own.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,21 +1,21 @@
 # Aliases
-alias g='git'
-alias gst='git status'
-alias gl='git pull'
-alias gup='git fetch && git rebase'
-alias gp='git push'
-alias gd='git diff | mate'
-alias gdv='git diff -w "$@" | vim -R -'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gb='git branch'
-alias gba='git branch -a'
-alias gcount='git shortlog -sn'
-alias gcp='git cherry-pick'
+#alias g='git'
+#alias gst='git status'
+#alias gl='git pull'
+#alias gup='git fetch && git rebase'
+#alias gp='git push'
+#alias gd='git diff | mate'
+#alias gdv='git diff -w "$@" | vim -R -'
+#alias gc='git commit -v'
+#alias gca='git commit -v -a'
+#alias gb='git branch'
+#alias gba='git branch -a'
+#alias gcount='git shortlog -sn'
+#alias gcp='git cherry-pick'
 
 
 # Git and svn mix
-alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
+#alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
 
 #
 # Will return the current branch name
@@ -27,6 +27,6 @@ function current_branch() {
 }
 
 # these aliases take advangate of the previous function
-alias ggpull='git pull origin $(current_branch)'
-alias ggpush='git push origin $(current_branch)'
-alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
+#alias ggpull='git pull origin $(current_branch)'
+#alias ggpush='git push origin $(current_branch)'
+#alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
